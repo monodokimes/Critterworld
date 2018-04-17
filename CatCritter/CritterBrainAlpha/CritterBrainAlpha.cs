9091811b9ba1c89f9a5ccd22f48a53b0ebb1d2bf
Cat Flynn <100458008@unimail.derby.ac.uk>
2018-04-17 10:25:26 +0100

    Load critter brain into world

---
+++ CatCritter/CritterBrainAlpha/CritterBrainAlpha.cs
@@ -0,0 +1,24 @@
+﻿using CritterBrains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace CatCritter
+{
+    public class CritterBrainAlpha : CritterBrain
+    {
+        public CritterBrainAlpha(string name, string creator) 
+            : base (name, creator)
+        {
+        }
+
+        public CritterBrainAlpha(string name, string creator, Image[] images) 
+            : base (name, creator, images)
+        {
+
+        }
+    }
+}
---
+++ CatCritter/CritterCreator.cs
@@ -0,0 +1,23 @@
+﻿using CritterBrains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatCritter
+{
+    public class CritterCreator : ICritterFactory
+    {
+        public IEnumerable<CritterBrain> GetCritterBrains()
+        {
+            var creator = "Cat Flynn 100458008";
+
+            var critters = new[] {
+                new CritterBrainAlpha("alpha", creator)
+            };
+
+            return critters;
+        }
+    }
+}
